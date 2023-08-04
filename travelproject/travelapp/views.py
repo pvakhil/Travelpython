285842020b48701b14c99c6dbddbe0eafb79f986
pvakhil <133202583+pvakhil@users.noreply.github.com>
2023-08-05 03:55:10 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,26 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place
+from . models import team
+
+
+def demo(request):
+     obj=place.objects.all()
+
+     obj1=team.objects.all()
+
+
+
+     return  render(request,"index.html",{'result':obj,'results':obj1})
+
+
+
+#def addition(request):
+    # x=int(request.GET['num1'])
+     #y=int(request.GET['num2'])
+     #res=x+y
+     #return render(request,"result.html",{'result':res})
+
+
+
